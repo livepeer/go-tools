b4d9ced059570e2dcd2746b1213c933127ad6d76
Rafał Leszko <rafal@livepeer.org>
2023-07-07 08:45:46 +0200

    Fix S3 support for Filebase (#68)
    Filebase requires setting "region" to "us-east-1" in the AWS Client configuration.

--- drivers/drivers_test.go
+++ drivers/drivers_test.go
@@ -71,7 +71,7 @@ func TestCustomS3URL(t *testing.T) {
 	assert.Equal("bucket-name", s3.bucket)
 	assert.Equal("user", s3.awsAccessKeyID)
 	assert.Equal("password", s3.awsSecretAccessKey)
-	assert.Equal("example", s3.region)
+	assert.Equal("us-east-1", s3.region)
 }
 
 func TestCustomS3URLWithRegion(t *testing.T) {
@@ -82,20 +82,21 @@ func TestCustomS3URLWithRegion(t *testing.T) {
 	assert.Equal(nil, err)
 	assert.Equal("http://example.com:9000", s3.host)
 	assert.Equal("bucket-name", s3.bucket)
-	assert.Equal("example", s3.region)
+	assert.Equal("us-east-1", s3.region)
 	assert.Equal("user", s3.awsAccessKeyID)
 	assert.Equal("password", s3.awsSecretAccessKey)
 }
 
 func TestCustomS3RegionParser(t *testing.T) {
 	cases := [][]string{
-		{"http://one:9000", "one"},
-		{"http://one.two:9000", "one"},
-		{"http://one.two.three:9000", "two"},
+		{"http://one:9000", "us-east-1"},
+		{"http://one.two:9000", "us-east-1"},
+		{"http://one.two.three:9000", "us-east-1"},
 		{"http://one.two.three.four:9000", "two"},
 		{"http://one.two.three.four.five:9000", "two"},
 		{"http://s3.eu-central-2.wasabisys.com", "eu-central-2"},
 		{"http://s3.us-east-2.amazonaws.com", "us-east-2"},
+		{"https://s3.filebase.com", "us-east-1"},
 	}
 	assert := assert.New(t)
 	for _, pair := range cases {
